7dba0fc51759bee857c320068d0039ff94a9556f
Garima Singh <garimasingh2023vns@gmail.com>
2024-02-09 18:47:22 +0530

    Garima

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,9 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel:2.3.1")
+    implementation ("androidx.lifecycle:lifecycle-livedata:2.3.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
--- app/src/main/java/com/drowsiness/ai/views/activities/GetStartedActivity.kt
+++ app/src/main/java/com/drowsiness/ai/views/activities/GetStartedActivity.kt
@@ -1,12 +1,91 @@
 package com.drowsiness.ai.views.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.viewpager2.widget.ViewPager2
 import com.drowsiness.ai.R
+import com.drowsiness.ai.databinding.ActivityGetStartedBinding
+import com.drowsiness.ai.databinding.ActivitySplashBinding
 
 class GetStartedActivity : AppCompatActivity() {
+
+    lateinit var getStartedBinding: ActivityGetStartedBinding
+    lateinit var iv1:ImageView
+    lateinit var iv2:ImageView
+    lateinit var iv3:ImageView
+    lateinit var viewPager2: ViewPager2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_get_started)
+        getStartedBinding = ActivityGetStartedBinding.inflate(layoutInflater)
+        setContentView(getStartedBinding.root)
+        getStartedBinding.button.setOnClickListener {
+//            var intent = Intent(this, SplashActivity::class.java)
+            startActivity(Intent(this,MainActivity::class.java))
+        }
+
+        getStartedBinding.button2.setOnClickListener {
+//            var intent = Intent(this, SplashActivity::class.java)
+            startActivity(Intent(this,MainActivity::class.java))
+        }
+
+
+
+        viewPager2=findViewById(R.id.view_pager2)
+        iv1=findViewById(R.id.iv1)
+        iv2=findViewById(R.id.iv2)
+        iv3=findViewById(R.id.iv3)
+
+        val images= listOf(R.drawable.phone,R.drawable.images,R.drawable.cardriving)
+        val adapter=ViewPagerAdapter(images)
+        viewPager2.adapter=adapter
+
+        viewPager2.registerOnPageChangeCallback(object :ViewPager2.OnPageChangeCallback(){
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                changeColor()
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+            }
+
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+                super.onPageScrollStateChanged(state)
+                changeColor()
+            }
+
+        })
+
+
+
+
+    }
+    fun changeColor(){
+        when(viewPager2.currentItem){
+            0->
+            {
+                iv1.setBackgroundColor(applicationContext.resources.getColor(R.color.active,theme))
+                iv2.setBackgroundColor(applicationContext.resources.getColor(R.color.red_200))
+                iv3.setBackgroundColor(applicationContext.resources.getColor(R.color.red_200))
+            }
+            1->
+            {
+                iv1.setBackgroundColor(applicationContext.resources.getColor(R.color.red_200))
+                iv2.setBackgroundColor(applicationContext.resources.getColor(R.color.active))
+                iv3.setBackgroundColor(applicationContext.resources.getColor(R.color.red_200))
+            }
+            2->
+            {
+                iv1.setBackgroundColor(applicationContext.resources.getColor(R.color.red_200))
+                iv2.setBackgroundColor(applicationContext.resources.getColor(R.color.red_200))
+                iv3.setBackgroundColor(applicationContext.resources.getColor(R.color.active))
+            }
+        }
     }
 }
